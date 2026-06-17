a8ba50cbee7348ee146a76c3cd0637c7bb850258
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-06-17 06:15:16 +0200

    feature: Use of elevator controller in the console.

--- src/ElevatorSim.Console/Program.cs
+++ src/ElevatorSim.Console/Program.cs
@@ -1,29 +1,33 @@
-﻿using ElevatorSim.Application.Strategies;
+﻿using ElevatorSim.Application.Services;
+using ElevatorSim.Application.Strategies;
 using ElevatorSim.Domain.Entities;
 using ElevatorSim.Domain.Interfaces;
+using ElevatorSim.Domain.Models;
 
 const int minFloor = 1;
 const int maxFloor = 10;
 const int maxNumberOfPeople = 10;
 
+
 // Mock elevator
-IElevator elevator = new PassengerElevator(minFloor,maxFloor,maxNumberOfPeople);
+IElevator elevator1 = new PassengerElevator(minFloor,maxFloor,maxNumberOfPeople);
+IElevator elevator2 = new PassengerElevator(minFloor, maxFloor, maxNumberOfPeople, 2);
 
-
+IElevator[] elevators = new[] { elevator1, elevator2 };
 // TODO: A main loop
 
-int passengerFloorNumber = GetPassengerFloorNumber();
-// Mock passenger
-Passenger passenger = new Passenger(passengerFloorNumber);
-// Call elevator
-IDispatchStrategy dispatchStrategy = new NearestAvailableStrategies();
-passenger.RequestElevator();
+IDispatchStrategy strategy = new NearestAvailableStrategies();
+ElevatorController elevatorController = new ElevatorController(maxFloor, 3, elevators.ToList(), strategy);
+
+FloorRequest fRequest = GetPassengerFloorNumber();
 
 
-static int GetPassengerFloorNumber()
+elevatorController.DispatchElevator(fRequest);
+
+static FloorRequest GetPassengerFloorNumber()
 {
     // Enter the floor within range
-    int desiredPassengerFloor;
+    int desiredPassengerFloor, numberOfPassengers;
 
     while (true)
     {
@@ -44,7 +48,20 @@ static int GetPassengerFloorNumber()
         break;
     }
 
-    return desiredPassengerFloor;
+    while (true)
+    {
+        Console.Write("Enter the number of passengers: ");
+        
+        if (!int.TryParse(Console.ReadLine(), out numberOfPassengers))
+        {
+            Console.WriteLine("Please enter a valid number.");
+            continue;
+        }
+        
+        break;
+    }
+
+    return new FloorRequest(desiredPassengerFloor, numberOfPassengers);
 }
 static bool IsFloorWithinRange(int desiredFloor, int minFloor, int maxFloor)
 {
