7214f7791879617df642aaae8b1dcc4e047f50c3
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-06-18 13:19:53 +0200

    chore: Commenting for both dispatch strategy and entity.

--- src/ElevatorSim.Domain/Interfaces/IEntity.cs
+++ src/ElevatorSim.Domain/Interfaces/IEntity.cs
@@ -1,6 +1,10 @@
 namespace ElevatorSim.Domain.Interfaces;
 
+/// <summary>
+/// Base contract for all identifiable domain entities.
+/// </summary>
 public interface IEntity
 {
+    /// <summary>Gets the unique identifier for this entity.</summary>
     public Guid Id { get; }
 }
