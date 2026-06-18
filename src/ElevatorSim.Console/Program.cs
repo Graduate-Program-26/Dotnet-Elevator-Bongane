e6f05e0646161ef1d9c486eee241b2af0311bef8
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-06-18 11:03:46 +0200

    feature: Remove mock data from program file and implement dependency injection not new objects.

--- src/ElevatorSim.Console/Program.cs
+++ src/ElevatorSim.Console/Program.cs
@@ -1,69 +1,29 @@
-﻿using ElevatorSim.Application.Services;
+using ElevatorSim.Application.Interfaces;
+using ElevatorSim.Application.Services;
 using ElevatorSim.Application.Strategies;
+using ElevatorSim.Console;
 using ElevatorSim.Domain.Entities;
 using ElevatorSim.Domain.Interfaces;
-using ElevatorSim.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 const int minFloor = 1;
 const int maxFloor = 10;
 const int maxNumberOfPeople = 10;
+const int maxElevators = 3;
 
+var services = new ServiceCollection();
 
-// Mock elevator
-IElevator elevator1 = new PassengerElevator(minFloor,maxFloor,maxNumberOfPeople);
-IElevator elevator2 = new PassengerElevator(minFloor, maxFloor, maxNumberOfPeople, 2);
+services.AddSingleton<IDispatchStrategy, NearestAvailableStrategies>();
 
-IElevator[] elevators = new[] { elevator1, elevator2 };
-// TODO: A main loop
-
-IDispatchStrategy strategy = new NearestAvailableStrategies();
-ElevatorController elevatorController = new ElevatorController(maxFloor, 3, elevators.ToList(), strategy);
-
-FloorRequest fRequest = GetPassengerFloorNumber();
-
-
-elevatorController.DispatchElevator(fRequest);
-
-static FloorRequest GetPassengerFloorNumber()
+services.AddSingleton<IElevatorController>(serviceProvider =>
 {
-    // Enter the floor within range
-    int desiredPassengerFloor, numberOfPassengers;
-
-    while (true)
+    var strategy = serviceProvider.GetRequiredService<IDispatchStrategy>();
+    var fleet = new List<IElevator>
     {
-        Console.Write("Enter your desired floor (1-10): ");
+        new PassengerElevator(minFloor, maxFloor, maxNumberOfPeople),
+        new PassengerElevator(minFloor, maxFloor, maxNumberOfPeople, 2),
+    };
+    return new ElevatorController(maxFloor, maxElevators, fleet, strategy);
+});
 
-        if (!int.TryParse(Console.ReadLine(), out desiredPassengerFloor))
-        {
-            Console.WriteLine("Please enter a valid number.");
-            continue;
-        }
 
-        if (!IsFloorWithinRange(desiredPassengerFloor, 1, 10))
-        {
-            Console.WriteLine("Floor must be between 1 and 10.");
-            continue;
-        }
-
-        break;
-    }
-
-    while (true)
-    {
-        Console.Write("Enter the number of passengers: ");
-        
-        if (!int.TryParse(Console.ReadLine(), out numberOfPassengers))
-        {
-            Console.WriteLine("Please enter a valid number.");
-            continue;
-        }
-        
-        break;
-    }
-
-    return new FloorRequest(desiredPassengerFloor, numberOfPassengers);
-}
-static bool IsFloorWithinRange(int desiredFloor, int minFloor, int maxFloor)
-{
-    return desiredFloor <= maxFloor && desiredFloor >= minFloor ;
-}
